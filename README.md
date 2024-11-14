68aa7a33d9f59082a1f702fa3b8ae56fd4d94975
Sikati Kenmogne Samuel <sikatikenmogne@gmail.com>
2024-11-14 18:58:39 +0100

    feat(serializers): add field validation in serializers and update documentation
    - Add validation for `username` and `email` fields in `UserSerializer`
    - Add validation for `closed` field in `TaskSerializer` to ensure it is `False`
    - Update `README.md` to include new validation rules

--- README.md
+++ README.md
@@ -47,9 +47,9 @@ You can access the API at http://127.0.0.1:8000/. The available endpoints are:
 - [x] Login endpoint to authenticate the user and generate a token
 - [x] CRUD operations for managing the to-do list
 - [x] Implement user authentication to allow only authorized users to access the to-do list
-- [ ] Implement error handling and security measures
+- [x] Implement error handling and security measures
 - [x] Use a database to store the user and to-do list data (you can use any database of your choice)
-- [ ] Implement proper data validation
+- [x] Implement proper data validation
 - [x] Implement pagination and filtering for the to-do list
 
 ## Pre-commit Hooks
--- task/serializers.py
+++ task/serializers.py
@@ -1,23 +1,67 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueTogetherValidator
 
 from task.models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Task model.
+    """
+
     class Meta:
         model = Task
-        fields = ["id", "title", "description"]
+        fields = ["id", "title", "description", "closed"]
+
+    def validate_closed(self, value):
+        """
+        Validate the 'closed' field to ensure it is False.
+
+        Args:
+            value (bool): The value of the 'closed' field.
+
+        Raises:
+            serializers.ValidationError: If the 'closed' field is True.
+
+        Returns:
+            bool: The validated value of the 'closed' field.
+        """
+        if value is True:
+            raise serializers.ValidationError("The finished attrribute must be False")
+        return value
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the User model.
+    """
+
     password = serializers.CharField(write_only=True)
 
     class Meta:
+        """
+        Meta class for UserSerializer.
+        """
+
         model = User
         fields = ["username", "email", "password"]
+        validators = [
+            UniqueTogetherValidator(
+                queryset=User.objects.all(), fields=["username", "email"]
+            )
+        ]
 
     def create(self, validated_data):
+        """
+        Create a new User instance.
+
+        Args:
+            validated_data (dict): The validated data containing user information.
+
+        Returns:
+            User: The created User instance.
+        """
         user = User.objects.create_user(
             username=validated_data["username"],
             email=validated_data["email"],
